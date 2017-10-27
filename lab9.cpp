403995265d6c35caf1cdd144f787f12e9b002d09
v6854384 <32385043+v6854384@users.noreply.github.com>
2017-10-27 11:02:35 +0300

    Create lab9.cpp

---
+++ lab9.cpp
@@ -0,0 +1,22 @@
+#include<iosteam>
+using namespace std;
+struct Node {
+        string data;
+        Node * next;
+}
+
+struct List {
+        Node * head;
+}
+bool simple_print(List list, string filter = "str1") {
+
+}
+bool reg.print(List list, string filter = "str1") {
+
+}
+
+int main () {
+        List list;
+        list.head = new Node{"str1", nullptr};
+        list.headnext = new Node{"str2", nullptr};
+}
